1a7d8f5afb30025cd566135bd72f86df5e364252
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-01-05 08:28:54 +0100

    Add cach

--- C4PHub.Web/Controllers/HomeController.cs
+++ C4PHub.Web/Controllers/HomeController.cs
@@ -18,15 +18,9 @@ namespace C4PHub.Web.Controllers
             _persistance = persistance;
         }
 
+        [ResponseCache(Duration = 60)]
         public async Task<IActionResult> Index()
         {
-            ////var url = "https://www.sessionize.com/future-tech-2024/";
-            //var url = "https://www.papercall.io/serverlessdays-milano-2024";
-
-            //var c4p = await _c4pManager.CreateC4PFromUrlAsync(url, default);
-
-            //if (!await _persistance.ExistsC4PAsync(c4p, default))
-            //    await _persistance.SaveC4PAsync(c4p, default);
             return View();
         }
 
