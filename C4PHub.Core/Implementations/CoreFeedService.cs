9b7156ade46c926048500dcc8b9ef483d6ad454b
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-01-08 16:52:01 +0100

    New feed service implementation

--- C4PHub.Core/Implementations/CoreFeedService.cs
+++ C4PHub.Core/Implementations/CoreFeedService.cs
@@ -1,10 +1,12 @@
-﻿using C4PHub.Core.Interfaces;
+﻿using C4PHub.Core.Entities;
+using C4PHub.Core.Interfaces;
 using C4PHub.RssApi.Writers;
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Atom;
 using Microsoft.SyndicationFeed.Rss;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -27,38 +29,51 @@ namespace C4PHub.Core.Implementations
 
             c4pList = c4pList.OrderByDescending(c => c.InsertDate);
 
-            var feed = new SyndicationFeed("C4PHub", "C4PHub", new Uri(host), "https://c4phub.com", DateTimeOffset.Now);
-
-            feed.Categories.Add(new SyndicationCategory("Public Speaking"));
-            feed.ImageUrl=new Uri("https://www.c4phub.com/img/c4phublogo.png");
-
-            var items = new List<SyndicationItem>();
-            foreach (var c4p in c4pList)
+            StringWriter sw = new StringWriter();
+            using (XmlWriter xmlWriter = XmlWriter.Create(sw,
+                new XmlWriterSettings() { Async = true, Indent = true }))
             {
-                var item = new SyndicationItem()
+                var rss = new RssFeedWriter(xmlWriter);
+                await rss.WriteTitle("C4PHub");
+                await rss.WriteDescription("The platform to share C4P with your community!");
+                await rss.WriteGenerator("C4PHub");
+                await rss.WriteValue("link", host);
+
+                if (c4pList != null && c4pList.Count() > 0)
                 {
-                    Id = c4p.Id,
-                    LastUpdatedTime = c4p.InsertDate,
-                    PublishDate = c4p.InsertDate
-                };
-                var summary= HttpUtility.HtmlEncode($"Call for paper for the event '{c4p.EventName}' expires on {c4p.ExpiredDate:dd/MM/yyyy}");
-                item.Summary = SyndicationContent.CreatePlaintextContent(summary );
-                var content= HttpUtility.HtmlEncode($"Call for paper of the event '{c4p.EventName}' which will be held on {c4p.EventDate:dd/MM/yyyy} at {c4p.EventLocation} expires on {c4p.ExpiredDate:dd/MM/yyyy}");
-                item.Content = SyndicationContent.CreatePlaintextContent(content);
-                item.Links.Add(new SyndicationLink(new Uri(c4p.Url)));
-                item.Authors.Add(new SyndicationPerson() { Name = c4p.UserPublished });
+                    var feedItems = new List<AtomEntry>();
+                    foreach (var c4p in c4pList)
+                    {
+                        var item = ToRssItem(c4p, host);
+                        feedItems.Add(item);
+                    }
 
-                items.Add(item);
+                    foreach (var feedItem in feedItems)
+                    {
+                        await rss.Write(feedItem);
+                    }
+                }
             }
-            feed.Items = items;
-
-            var sw = new StringWriterWithEncoding(Encoding.UTF8);
-            using (XmlWriter writer = XmlWriter.Create(sw, new XmlWriterSettings { Indent = true, Encoding = Encoding.UTF8 }))
-            {
-                feed.SaveAsRss20(writer);
-            }
-
             return sw.ToString();
+        }
+
+        private AtomEntry ToRssItem(C4PInfo c4pInfo, string host)
+        {
+            var item = new AtomEntry
+            {
+                Title = HttpUtility.HtmlEncode(c4pInfo.EventName),
+                Description = HttpUtility.HtmlEncode($"Call for paper of the event '{c4pInfo.EventName}' which will be held on {c4pInfo.EventDate:dd/MM/yyyy} at {c4pInfo.EventLocation} expires on {c4pInfo.ExpiredDate:dd/MM/yyyy}"),
+                Id = c4pInfo.Id,
+                Published = c4pInfo.InsertDate,
+                LastUpdated = c4pInfo.InsertDate,
+                ContentType = "html",
+            };
+
+            item.AddContributor(new SyndicationPerson(c4pInfo.UserPublished, c4pInfo.UserPublished));
+
+            item.AddLink(new SyndicationLink(new Uri(c4pInfo.Url)));
+
+            return item;
         }
     }
 }
