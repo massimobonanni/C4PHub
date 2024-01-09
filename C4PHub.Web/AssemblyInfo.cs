88410062010e1036ca11413db7ffd64603690ab0
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-01-09 17:18:06 +0100

    Add assembly info

---
+++ C4PHub.Web/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Massimo Bonanni")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("C4PHub")]
+[assembly: System.Reflection.AssemblyTitleAttribute("C4PHub.Web")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
