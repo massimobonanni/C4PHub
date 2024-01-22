22b88203f08cf2793b6cfb4f4b3f88d3196af977
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-01-22 16:09:21 +0100

    Fix AssemblyInfo for versioning

--- C4PHub.Web/AssemblyInfo.cs
+++ C4PHub.Web/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Massimo Bonanni")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("C4PHub")]
-[assembly: System.Reflection.AssemblyTitleAttribute("C4PHub.Web")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyCompany("Massimo Bonanni")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyProduct("C4PHub")]
+[assembly: System.Reflection.AssemblyTitle("C4PHub.Web")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
