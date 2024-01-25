1ccd67f802968d087389a518f07826225b20099a
Massimo Bonanni <massimo.bonanni@tiscali.it>
2024-01-25 16:13:55 +0100

    Add calendar generator interface

---
+++ C4PHub.Core/Entities/CalendarType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4PHub.Core.Entities
+{
+    public enum  CalendarType
+    {
+        EventDate,
+        C4PExpirationDate
+    }
+}
---
+++ C4PHub.Core/Interfaces/IC4PCalendarGenerator.cs
@@ -0,0 +1,14 @@
+﻿using C4PHub.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4PHub.Core.Interfaces
+{
+    public interface IC4PCalendarGenerator
+    {
+        Task<string> GenerateAsync(C4PInfo c4p, CalendarFormat format, CalendarType type, CancellationToken cancellationToken = default);
+    }
+}
